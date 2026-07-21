896e4a81ee8a3f43cd1d2e69158143f170c44f87
erik <erik@erik.xyz>
2026-07-21 21:48:14 +0800

    feat: add EagerStrategy for ConnectionManager

---
+++ src/Connection/Strategy/EagerStrategy.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace IndustrialProtocols\Connection\Strategy;
+
+use IndustrialProtocols\Protocol\ConnectorInterface;
+
+class EagerStrategy implements StrategyInterface
+{
+    /** @var array<string, ConnectorInterface> */
+    private array $connections = [];
+
+    public function getOrCreate(string $deviceId, callable $factory): ConnectorInterface
+    {
+        if (!isset($this->connections[$deviceId])) {
+            $connector = $factory();
+            $connector->connect();
+            $this->connections[$deviceId] = $connector;
+        }
+        return $this->connections[$deviceId];
+    }
+
+    public function disconnect(string $deviceId): void
+    {
+        if (isset($this->connections[$deviceId])) {
+            $this->connections[$deviceId]->disconnect();
+            unset($this->connections[$deviceId]);
+        }
+    }
+
+    public function disconnectAll(): void
+    {
+        foreach ($this->connections as $id => $connector) {
+            $connector->disconnect();
+            unset($this->connections[$id]);
+        }
+    }
+
+    public function getActiveConnections(): array
+    {
+        return $this->connections;
+    }
+}
---
+++ tests/Unit/EagerStrategyTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace IndustrialProtocols\Tests\Unit;
+
+use IndustrialProtocols\Connection\Strategy\EagerStrategy;
+use IndustrialProtocols\Protocol\ConnectorInterface;
+use PHPUnit\Framework\TestCase;
+
+class EagerStrategyTest extends TestCase
+{
+    public function testEagerStrategyCallsConnectImmediately(): void
+    {
+        $connector = $this->createMock(ConnectorInterface::class);
+        $connector->expects($this->once())->method('connect');
+
+        $strategy = new EagerStrategy();
+        $result = $strategy->getOrCreate('dev-1', function () use ($connector) {
+            return $connector;
+        });
+
+        $this->assertSame($connector, $result);
+    }
+
+    public function testEagerStrategyReusesExistingConnection(): void
+    {
+        $connector = $this->createMock(ConnectorInterface::class);
+        $connector->expects($this->once())->method('connect');
+
+        $strategy = new EagerStrategy();
+        $strategy->getOrCreate('dev-1', fn() => $connector);
+
+        $connector2 = $this->createMock(ConnectorInterface::class);
+        $connector2->expects($this->never())->method('connect');
+
+        $result = $strategy->getOrCreate('dev-1', fn() => $connector2);
+        $this->assertSame($connector, $result);
+    }
+
+    public function testEagerStrategyDisconnect(): void
+    {
+        $connector = $this->createMock(ConnectorInterface::class);
+        $connector->expects($this->once())->method('disconnect');
+
+        $strategy = new EagerStrategy();
+        $strategy->getOrCreate('dev-1', fn() => $connector);
+        $strategy->disconnect('dev-1');
+
+        $this->assertEmpty($strategy->getActiveConnections());
+    }
+}
